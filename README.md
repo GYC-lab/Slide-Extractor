4de746228ae6f72b4a26227b53fa2089a4638e8a
GYC <yuchen.ge@stu.pku.edu.cn>
2023-02-10 14:23:31 +0800

    add feature: handle multi videos

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
 # Slide-Extractor
-Extracts slides from a video of a ppt presentation
+Extracts slides from videos of ppt presentations rather than a single video
 ## How to run
-1. navigate to the file directory through terminal or cmd
-2. Run the requirements.txt
-  ```
-  pip install -r requirements.txt
-  ```
-3. run the program in the below format
-  ```
-  python slides.py <videopath> -f <output folder name> 
-  ```
-4. optionaly if you want to adjust the number of frames skipped to speed up the process(default is 5) you can use
-  ```
-  python slides.py <videopath> -f <output folder name> -s <number of frames to be skipped>
-  ```
-5. optionaly if you want to adjust the simalirity threshold which is a float value ranging from 0-1 and higher the value is the code will only detect more dissimilar frames to adjust this value you can use
-  ```
-  python slides.py <videopath> -f <output folder name> -p <a float value b/w 0-1>
-  ```
+1. put your videos of ppt presentations into the folder `input_video`
+2. run `slides.py` with 
+   ```
+    python slides.py
+   ```
+   you can be informed of the progress from the output progressbar
+3. when the progress is done, you can check the folder `output_slide` which contains the extracted slides
+4. you may need to select the slides since the code will capture any difference between frames in videos
--- slides.py
+++ slides.py
@@ -1,75 +1,75 @@
 import numpy as np
 import progressbar
 import cv2
-import time
-import argparse 
-import sys
 import os
 
-#using argprase to do saving,giving animal and habitat model and to give arena pic
-parser = argparse.ArgumentParser( usage="""python myscript.py {vid}""",description='''Description.''',epilog="""Epilog.""")
-parser.add_argument("vid", help="input path")
-parser.add_argument('-f',help="folder path for saving")
-parser.add_argument('-s',help="no of frames to be skipped")
-parser.add_argument('-p',help="fintune the threshold percentage(0-1 float values)")
-args = parser.parse_args()
-if(args.vid==None or args.f==None):
-	print("ERROR:no input/output path specified")
-	exit()
-input_location=args.vid
-output_location=args.f
-perc=0.15
-ch=5
-if(args.s!=None):
-    ch=int(args.skip)
-if(args.p!=None):
-    perc=float(args.skip)
+def video2slide(input_video,final_directory,perc,ch):
+    
+    cap = cv2.VideoCapture(input_video)
 
-current_directory = os.getcwd()
-final_directory = os.path.join(current_directory,output_location)
-if not os.path.exists(final_directory):
-   os.makedirs(final_directory)
+    if cap.isOpened()==False:
+        print("ERROR:Failed to open video file")
+        exit()
 
-cap = cv2.VideoCapture(input_location)
+    os.chdir(final_directory)
+    count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    n=cap.get(cv2.CAP_PROP_FRAME_WIDTH)*cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    old_frame = None
+    f=0
+    i=0
+    sl_count=1
 
-if cap.isOpened()==False:
-	print("ERROR:Failed to open video file")
-	exit()
-os.chdir(final_directory)
-count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-n=cap.get(cv2.CAP_PROP_FRAME_WIDTH)*cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-old_frame = None
-f=0
-i=0
-sl_count=1
+    bar = progressbar.ProgressBar(maxval=count, \
+        widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+    bar.start()
 
-bar = progressbar.ProgressBar(maxval=count, \
-    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-bar.start()
+    while i<count:
 
-while i<count:
+        ret, frame = cap.read()
+        f=f+1
+        if ret == True and f==ch:
 
-    ret, frame = cap.read()
-    f=f+1
-    if ret == True and f==ch:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        if old_frame is not None:
-            diff_frame = cv2.absdiff(frame, old_frame)
-            diff_per=float(cv2.countNonZero(cv2.cvtColor(diff_frame, cv2.COLOR_BGR2GRAY))/n)
-            frame_blank=float(cv2.countNonZero(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))/n)
-            if diff_per>perc and frame_blank>0.40:
-                name=('slide' + "% d"+'.jpg') % sl_count
-                cv2.imwrite(name,frame)
-                sl_count=sl_count+1
+            if old_frame is not None:
+                diff_frame = cv2.absdiff(frame, old_frame)
+                diff_per=float(cv2.countNonZero(cv2.cvtColor(diff_frame, cv2.COLOR_BGR2GRAY))/n)
+                frame_blank=float(cv2.countNonZero(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))/n)
+                if diff_per>perc and frame_blank>0.40:
+                    name=('slide' + "% d"+'.jpg') % sl_count
+                    cv2.imwrite(name,frame)
+                    sl_count=sl_count+1
+                    old_frame = frame
+            else:
                 old_frame = frame
-        else:
-            old_frame = frame
-        f=0
-    bar.update(i+1)
-    i=i+1
+            f=0
+        bar.update(i+1)
+        i=i+1
+    print("capture %d slides"%sl_count)
+    bar.finish()
+    cap.release()
 
-bar.finish()
-cap.release()
+if __name__ == "__main__":
+    
+    folderpath = os.getcwd()
+    path_in = folderpath + "\input_video\\"
+    path_out = folderpath + "\output_silde\\"
+    files = os.listdir(path_in)
 
+    perc=0.25   
+    ch=25       
+
+    for file in files:
+        input_location=path_in+file
+        output_location=path_out+file
+        i = output_location.rfind('.')
+        output_location = output_location[:i]
+
+        final_directory = os.path.join(folderpath,output_location)
+
+        if not os.path.exists(final_directory):
+            os.makedirs(final_directory)
+
+        print("handling "+file)
+        input_location=path_in+file
+        video2slide(input_location,final_directory,perc,ch)
